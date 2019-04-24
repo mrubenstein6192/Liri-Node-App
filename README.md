1104065a96158d179f17e3fb374edd80562a44ad
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-04-24 19:34:26 -0400

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Liri-Node-App
+
+## Functionality
+
+The Liri app will allow users to seek more information from either a song name, artist he/she wishes to see in concert, or a movie.  
+
+After typing "node liri.js", the user will input one of three commands:
+
+### spotify-this-song
+
+The user will input a song and retrieve the artits name, album name, and a preview URL of the song.
+
+![spotify-this-song image]("./screenshots/spotify-this-song.png)
--- liri.js
+++ liri.js
@@ -16,67 +16,74 @@ var fs = require("fs");
 var moment = require("moment");
 
 //switch statement
-switch(process.argv[2]) {
-  case "spotify-this-song": 
-  spotifyFunction();
-  break;
+switch (process.argv[2]) {
+  case "spotify-this-song":
+    spotifyFunction();
+    break;
 
   case "concert-this":
-  concertFunction();
-  break;
+    concertFunction();
+    break;
 
   case "movie-this":
-  movieFunction();
-  break;
+    movieFunction();
+    break;
 
   case "do-what-it-says":
-  justDoItFunction();
-  break;
+    justDoItFunction();
+    break;
 }
 
 //concert function
-function concertFunction(){
+function concertFunction() {
   axios
-  .get("https://rest.bandsintown.com/artists/" + process.argv.slice(3).join("+") + "/events?app_id=codingbootcamp")
-  .then(
-    function(response){
-    // console.log(response.data[0]);
-    console.log(`
+    .get("https://rest.bandsintown.com/artists/" + process.argv.slice(3).join("+") + "/events?app_id=codingbootcamp")
+    .then(
+      function (response) {
+        // console.log(response.data[0]);
+        console.log(`
     Artist: ${process.argv.slice(3).join(" ").toUpperCase()}
-    Tour: ${response.data[0].venue.name}
+    Venue/Tour: ${response.data[0].venue.name}
     Location: ${response.data[0].venue.city}
     Date: ${moment(response.data[0].datetime).format("dddd, MMMM Do YYYY, h:mm a")}
     `);
-  })
-  .catch(function(err){
-    console.log(err) 
-    console.log("\nResults Not Found. This artist is either dead or has not scheduled concerts in the near future.");
-  });
+      })
+    .catch(function (err) {
+      console.log(err)
+      console.log("\nResults Not Found. This artist is either dead or has not scheduled concerts in the near future.");
+    });
 };
 
 //spotifyFunction
 function spotifyFunction() {
-spotify.search({type: 'track', query: process.argv.slice(3).join("+"), limit: 1}, function(err, response) {
-  if (err) {
-    return console.log("Erorr:" + err);
-  }
-console.log(response.tracks.items[0]);
-console.log(`
+  var song = process.argv.slice(3).join("+")
+  spotify.search({
+    type: 'track',
+    query: song,
+    limit: 1
+  }, function (err, response) {
+    if (err) {
+      return console.log("Erorr:" + err + "\nCheck your spelling!");
 
+    }
+    // console.log(); used this to dive into the objects
+    console.log(`
+Artist: ${response.tracks.items[0].album.artists[0].name}
 Song: ${response.tracks.items[0].name}
-
+Preview URL: ${response.tracks.items[0].preview_url}
+Album: ${response.tracks.items[0].album.name}
 
 `)
-});
+  });
 };
 
 //movie function
 function movieFunction() {
   axios
-  .get("https://www.omdbapi.com/?apikey=trilogy&t=" + process.argv.slice(3).join("+"))
-  .then(function(response) {
-    // console.log(response);
-    console.log(`
+    .get("https://www.omdbapi.com/?apikey=trilogy&t=" + process.argv.slice(3).join("+"))
+    .then(function (response) {
+      // console.log(response);
+      console.log(`
     Title: ${response.data.Title}
     Release Year: ${response.data.Year}
     IMDB Rating: ${response.data.imdbRating}
@@ -86,16 +93,19 @@ function movieFunction() {
     Plot: ${response.data.Plot}
     Actors: ${response.data.Actors}
     `);
-  }).catch(function(err){
-    console.log(err);
-  })
-  };
-
-  function justDoItFunction(){
-    fs.readFile("./random.txt", "utf8", function(err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(data);
+    }).catch(function (err) {
+      console.log(err);
     })
-  }
+};
+
+function justDoItFunction() {
+  fs.readFile("./random.txt", "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    var searchTerm = data.split(",");
+    console.log(searchTerm);
+    console.log("Type: `node liri.js ' and then the two above arguments with no other punctuation.")
+  })
+}
